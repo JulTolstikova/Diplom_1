32a860545ea065b636edcd6f6209de58efe75eac
Julia Tolstikova <jul.tolstickova@yandex.com>
2024-11-24 21:41:49 +0300

    добавила описания для test_ingridient

--- tests/test_ingridient.py
+++ tests/test_ingridient.py
@@ -4,28 +4,43 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FI
 
 class Test_ingridient():
     def test_get_name(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Ketchup", 1.5)
-        assert ingredient.get_name() == "Ketchup", "Метод get_name() должен возвращать 'Ketchup'"
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Ketchup", 50)
+        # Проверяю, что метод возвращает верное имя
+        assert ingredient.get_name() == "Ketchup"
 
     def test_get_price(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Cheese", 2.0)
-        assert ingredient.get_price() == 2.0, "Метод get_price() должен возвращать 2.0"
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Cheese", 70)
+        # Проверяю, что метод возвращает верную цену
+        assert ingredient.get_price() == 70
 
     def test_get_type(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Mayonnaise", 1.0)
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Mayonnaise", 50)
+        # Проверяю, что метод возвращает верный тип
         assert ingredient.get_type() == "SAUCE", "Метод get_type() должен возвращать 'SAUCE'"
 
     def test_change_name(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Bacon", 3.0)
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Bacon", 90)
+        # Меняю имя ингридиента
         ingredient.name = "Turkey"
-        assert ingredient.get_name() == "Turkey", "После изменения name, get_name() должен возвращать 'Turkey'"
+        # Проверяю, что возвращается измененное имя
+        assert ingredient.get_name() == "Turkey"
 
     def test_change_price(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Bacon", 3.0)
-        ingredient.price = 4.5
-        assert ingredient.get_price() == 4.5, "После изменения price, get_price() должен возвращать 4.5"
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Bacon", 90)
+        # Меняю цену
+        ingredient.price = 120
+        # Проверяю, что возвращается измененная цена
+        assert ingredient.get_price() == 120
 
     def test_change_type(self):
-        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Lettuce", 0.5)
+        # Создаю ингридиент с определенными параметрами
+        ingredient = Ingredient(INGREDIENT_TYPE_FILLING, "Lettuce", 30)
+        # Меняю тип ингридиента
         ingredient.type = INGREDIENT_TYPE_SAUCE
-        assert ingredient.get_type() == INGREDIENT_TYPE_SAUCE, "После изменения type, get_type() должен возвращать 'SAUCE'"
+        # Проверяю, что возвращается измененный тип ингридиента
+        assert ingredient.get_type() == INGREDIENT_TYPE_SAUCE
